287ed73b9739410a380cad4f4b80edda5f7cd20e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:29:20 +0530

    Add files via upload

---
+++ WaterIntakeManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WaterIntakeManagement.MvcApplication" Language="C#" %>
---
+++ WaterIntakeManagement/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace WaterIntakeManagement.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WaterIntakeManagement.DAL.WaterIntakeManagementDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(WaterIntakeManagement.DAL.WaterIntakeManagementDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
